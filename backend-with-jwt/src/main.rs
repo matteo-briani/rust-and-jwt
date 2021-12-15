f30875456efe233a88d018671ad5138943765683
spalla <spalla@autistici.org>
2021-12-15 16:49:40 +0100

    add diesel

--- backend-with-jwt/src/main.rs
+++ backend-with-jwt/src/main.rs
@@ -1,15 +1,36 @@
-use actix_web::{web, App, HttpServer};
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
+
+use actix_web::{dev::ServiceRequest, web, App, HttpServer};
+use diesel::prelude::*;
+use diesel::r2d2::{self, ConnectionManager};
 
 mod handlers;
+mod errors;
+mod models;
+mod schema;
+
+//custom type for convenience
+pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
 
+    dotenv::dotenv().ok();
     std::env::set_var("RUST_LOG", "actix_web=debug");
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE URL must be set");
+
+    // Create db connection pool
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool: Pool = r2d2::Pool::builder() 
+        .build(manager)
+        .expect("Failed to create pool.");
 
     // Start http server
     HttpServer::new(move || {
         App::new()
+            .data(pool.clone())
             .route("/users", web::get().to(handlers::get_users))
             .route("/users/{id}", web::get().to(handlers::get_user_by_id))
             .route("/users", web::post().to(handlers::add_user))
